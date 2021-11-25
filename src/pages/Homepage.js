f1e51b4427c7f2c8d8711b985181186344789323
Keveluk <carlos.stefano@hotmail.com.br>
2021-11-25 18:24:47 -0300

    corrigi erro homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,26 +5,26 @@ import {useNavigate} from 'react-router-dom'
 
 export default function Homepage() {
 
-    useEffect(() => {
-    // Atualiza o titulo do documento usando a API do browser
-    try{
-        await axios.get('http://localhost:1337/auth/local', credentials)
-        .then(response=>{
-                const cookies = new Cookies();
-                cookies.set('jwt', response.data.jwt, { path: '/' });
-                console.log(cookies.get('jwt'))
+//     useEffect(() => {
+//     // Atualiza o titulo do documento usando a API do browser
+//     try{
+//         await axios.get('http://localhost:1337/auth/local', credentials)
+//         .then(response=>{
+//                 const cookies = new Cookies();
+//                 cookies.set('jwt', response.data.jwt, { path: '/' });
+//                 console.log(cookies.get('jwt'))
                 
-                navigate('/Home')
+//                 navigate('/Home')
     
-        })
-        }catch(error){
-            console.log('Usuário ou senha inválido')
-        }
-    document.title = `Você clicou ${count} vezes`;
-  });
-    const isAuth = ()=>{
+//         })
+//         }catch(error){
+//             console.log('Usuário ou senha inválido')
+//         }
+//     document.title = `Você clicou ${count} vezes`;
+//   });
+//     const isAuth = ()=>{
 
-    }
+//     }
     return (
         <div>
             <h1>Homepage</h1>
